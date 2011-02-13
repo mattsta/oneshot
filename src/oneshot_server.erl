1a647f33179ecb5c61cbaee40b52c025337bd2fe
Matt Stancliff <matt@genges.com>
2011-02-12 22:21:55 -0800

    Add type information to state record

--- src/oneshot_server.erl
+++ src/oneshot_server.erl
@@ -6,7 +6,9 @@
 -export([handle_call/3, handle_cast/2, handle_info/2]).
 -export([terminate/2, code_change/3]).
 
--record(state, {listener, acceptor, module}).
+-record(state, {listener  :: pid(),
+                 acceptor :: any(), % opaque identifier
+                 module   :: atom() | fun()}).
 
 start_link(IP, Port, ModuleOrFun) -> 
   gen_server:start_link(?MODULE, [IP, Port, ModuleOrFun], []).
